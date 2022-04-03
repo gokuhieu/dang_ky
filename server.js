dad681073df9a145c3e4fb1361e6e7a7841d55c5
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2022-04-03 22:10:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -308,9 +308,9 @@ bot.on('messageCreate', (message) => {
 			})
 		}else if(message.content.toLowerCase().startsWith(`;coin`)){
 			var words =message.content.toLowerCase().split(" ")
-			if(words[1]==="" &&words[2]==="")
+			if(words[1]==="" &&words[2]===""&&message.member.roles.cache.some(role =>{role.id==="914502815060230204"}))
 			{
-				
+				message.channel.send("bạn là admin")
 			}else{
 				var query2 ="select * from public.user";
 				myconect.query(query2,(err,result) =>{
