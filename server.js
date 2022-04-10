033bf220672a02a8414be769b8307ff6ca4c19a9
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2022-04-10 23:56:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -462,7 +462,7 @@ bot.on('messageCreate', (message) => {
 		message.channel.send(item);
 		}
 		else if(message.content.toLowerCase().includes("https://discord.gg")){
-			message.channel.send("tin nhắn đã bị chặn do quảng cáo")
+			message.reply("tin nhắn đã bị chặn do quảng cáo")
 			.then(msg => {
 			  
 			  setTimeout(() => {message.delete()}, 100)
