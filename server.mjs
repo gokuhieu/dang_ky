ab66a234d6badae197a2dd6206270dfb441754bf
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2022-04-12 03:39:40 +0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -154,10 +154,24 @@ bot.on('guildMemberAdd', (member) => {
         channel.send(welcomeMessage)
     });
 });
+
 bot.on('ready', () => {
 	console.log('Ready!');
 });
+bot.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	const command = bot.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 function formathour(hour,min){
 if(min>=60){
 	return hour+1
