6c8b06ef330afe5c96663c9e9f33638b89fb8363
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2022-04-11 21:17:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -458,6 +458,8 @@ bot.on('messageCreate', (message) => {
 		else if(message.content.toLowerCase().includes("https://discord.gg")){
 			var admin =message.member.roles.cache.find(role => role.id === '914502815060230204')
 			if(message.member.roles.cache.has(admin)){
+
+			}else{
 				message.reply("tin nhắn đã bị chặn do quảng cáo").then(msg => {
 					setTimeout(() => {message.delete()},1000)
 					})
