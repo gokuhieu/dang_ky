dc158d1533abc3a5431a56ce56d2bb1c82aee795
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2022-04-12 02:59:26 +0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -15,8 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/public',express.static((__dirname+ '/public')))
 app.use(fileUpload({useTempFiles: true}))
 import { Client, Intents } from'discord.js';
+import token from `./Config.json` assert  { type: "json" };
 
-var token = import("Config.json", { assert: { type: "json" } });
 import village from'./village.json'
 import spawm  from'./spawm.json'
 import Connection from'pg';
