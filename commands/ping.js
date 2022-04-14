82bd1807edf20b4d2b7f971e37931f43dccc8134
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2022-04-15 03:54:02 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,29 @@
-import {ICommand} from 'wokcommands'
 module.exports ={
-    category: 'test',
+    category: 'Testing',
     description: 'đang xử lý',
     slash:"both",
     testOnly: true,
-    callback : ({ })=>{
-        return "pong"
-    }
+    callback: ({ message, interaction }) => {
+        const reply = 'Pong!'
+    
+        // message is provided only for a legacy command
+        if (message) {
+          message.reply({
+            content: reply
+          })
+          return
+        }
+    
+        // interaction is provided only for a slash command
+        interaction.reply({
+          content: reply
+        })
+        
+        // Alternatively we can just simply return our reply object
+        // OR just a string as the content.
+        // WOKCommands will handle the proper way to reply with it
+        return {
+          content: reply
+        }
+      },
 }
