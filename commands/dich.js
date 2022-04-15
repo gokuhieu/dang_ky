815ed2c771bdbc61f0de1a152ad142434f1ff402
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2022-04-16 00:19:46 +0700

    Update dich.js

--- commands/dich.js
+++ commands/dich.js
@@ -7,14 +7,21 @@ export default v ={
     testOnly: true,
     expectedArgs: '<chữ cần dịch>',
     minArgs: 1,
-    maxArgs: 1,
+    maxArgs: 100,
     syntaxError: 'Incorrect usage! Please use help command',
     callback: ({ channel,args,interaction  }) => {
-    
+        let s =""
+        for(let i=0;i<=99;i++){
+            if(args[i]){
+                s=s+args[i]
+            }else{
+                break;
+            }
+        }
         // message is provided only for a legacy command
         
         const encodedParams = new URLSearchParams();
-        encodedParams.append("q", args[0]);
+        encodedParams.append("q", x);
         encodedParams.append("target", "vi");
         encodedParams.append("source", "en");
 
